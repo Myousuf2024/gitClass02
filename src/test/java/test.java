491f8c67b72c8fdf1ba7a2cd523d0120f490f22c
Myousuf2024 <y.yousuf100200@gmail.com>
2024-08-09 23:46:23 +0200

    second commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,6 +1,11 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("this is the project version 1");
+        System.out.println("this is the project version 2");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        
     }
 }
