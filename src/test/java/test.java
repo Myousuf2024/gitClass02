0f78fcccf03f8fa66d5570e5fe074e71d4119eba
Myousuf2024 <y.yousuf100200@gmail.com>
2024-08-11 00:45:18 +0200

    fourth commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,11 +1,9 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("this is the project version 2");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        
+
+        System.out.println("this is the project version 4");
+
+
+
     }
 }
